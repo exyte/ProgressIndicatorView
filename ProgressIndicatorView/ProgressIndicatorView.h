3cbef5f988d03a4211b17f55458c48f0a5ea443e
Daniil Manin <manindaniil@gmail.com>
2021-10-07 15:20:47 +0700

    Initial Commit

---
+++ ProgressIndicatorView/ProgressIndicatorView.h
@@ -0,0 +1,18 @@
+//
+//  ProgressIndicatorView.h
+//  ProgressIndicatorView
+//
+//  Created by Daniil Manin on 07.10.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ProgressIndicatorView.
+FOUNDATION_EXPORT double ProgressIndicatorViewVersionNumber;
+
+//! Project version string for ProgressIndicatorView.
+FOUNDATION_EXPORT const unsigned char ProgressIndicatorViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProgressIndicatorView/PublicHeader.h>
+
+
